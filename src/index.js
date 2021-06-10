e5dc9386681ba9280d78795c5d714dac5e211ba4
pbsmith82 <pbsmith82@gmail.com>
2021-06-10 18:05:16 -0400

    commit tree cf45845cf30d479f24a02b7e9cc76ca078be5266

--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux'; /* code change */
+import devToolsEnhancer from 'remote-redux-devtools';
+import { Provider } from 'react-redux';
 import shoppingListItemReducer from './reducers/shoppingListItemReducer.js';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(shoppingListItemReducer, devToolsEnhancer())
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root')
+);
